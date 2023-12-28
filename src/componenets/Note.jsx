e71788267b4efa1b0db37a40660d069cc05f0bdb
Mazen Mohamed <mazenmezomohammed@gmail.com>
2023-12-28 22:35:18 +0200

    Google keep clone

---
+++ src/componenets/Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum
+        molestias optio adipisci odit numquam nam cumque pariatur eos ex
+        voluptatem vel cupiditate obcaecati voluptatum, unde vitae, possimus
+        beatae sapiente rem?
+      </p>
+    </div>
+  );
+}
+
+export default Note;
